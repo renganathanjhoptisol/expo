ad7e2cedfe41524e44e38061ddd46c9e84b0f8a7
Renganathan J H <renganathan.jh@optisolbusiness.com>
2021-05-24 16:04:35 +0530

    HelloWorldApplication First Commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!</Text>
+      <Text style={styles.boldText}>Hello World!!!, I am renganathan</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boldText : {
+    fontWeight : 'bold',
+    padding : 20,
+    backgroundColor : 'pink',
+  }
 });
