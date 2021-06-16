8ab0e374dc13404f2e4f71a04cdfc8ad22a3effd
Renganathan J H <renganathan.jh@optisolbusiness.com>
2021-06-16 17:18:24 +0530

    code

--- components/FlatList.js
+++ components/FlatList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, FlatList, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, Alert} from 'react-native';
 import {styles} from './Styles/Fstyle';
 
 
